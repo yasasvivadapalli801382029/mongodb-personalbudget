29f922a173cd9c9e6ba727964502a3330e463a2f
yasasvivadapalli801382029 <yasasvi09@gmail.com>
2024-09-14 20:31:54 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3100;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 25
+        },
+        {
+            title: 'Rent',
+            budget: 275
+        },
+        {
+            title: 'Grocery',
+            budget: 110
+        },
+    ]
+};
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})

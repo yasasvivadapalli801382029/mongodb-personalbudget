242c0e9343cbbcd10c05eaa203bdfb204d5868f1
yasasvivadapalli801382029 <yasasvi09@gmail.com>
2024-09-14 20:56:48 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3100;
+const fs = require("fs");
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello world');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile("budgetdata.json", "utf8", (err, data) => {
+        if (err) {
+          res.status(500).send("Error loading budget data");
+          return;
+        }
+        const budgetData = JSON.parse(data);
+        res.json(budgetData);
+      });
 });
 
 app.listen(port, () => {
